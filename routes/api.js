29bb37911ee852f3a600add200a4643bbb572586
orderku <63989360+orderku@users.noreply.github.com>
2021-12-06 15:31:32 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3201,8 +3201,7 @@ router.get('/toattp', async (req, res, next) => {
 	if(!apikeyInput) return res.json(loghandler.notparam)
 	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
         if (!url) return res.json(loghandler.noturl)
-	if (!url.startsWith('https')) return res.json(loghandler.invalidLink)
-
+	
 try {
    axios.get(`https://ezgif.com/gif-to-webp?url=${url}`).then(({ data }) => {
            var $ = cheerio.load(data)
